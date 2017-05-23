bbfd77515d25ada345b6ec9c4e503b77f3cbabe0
Ray Riffy <rayriffy@gmail.com>
2017-05-23 11:36:12 +0700

    Added Service Worker (offline-support)
    This commit will make web app run in offline and cache static files to
    makes web loads faster!

---
+++ sw.js
@@ -0,0 +1,35 @@
+importScripts('js/sw-toolbox.js');
+
+toolbox.precache([
+  'offline.html', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css', 'js/sw-toolbox.js', 'js/materialize.js','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js','https://fonts.googleapis.com/icon?family=Material+Icons',
+  'img/loader.gif','img/ico.png','manifest.json'
+])
+
+toolbox.options.debug = false;
+toolbox.options.cache.name = "static-v1";
+
+self.addEventListener('install', function install() {
+  self.skipWaiting();
+})
+self.addEventListener('activate', function activate(e) {
+  e.waitUntil(self.clients.claim())
+})
+
+toolbox.router.get("(.*)", function get(req, vals, opts) {
+  return toolbox.networkFirst(req, vals, opts)
+    .catch(function(error) {
+      console.log({req, vals, opts, error})
+      if (req.method === "GET" && req.headers.get("accept").includes("text/html")) {
+        return toolbox.cacheOnly(new Request("offline.html"), vals, opts)
+      }
+      throw error
+    })
+})
+
+toolbox.router.get("css/*.css", toolbox.fastest)
+toolbox.router.get("js/*.js", toolbox.fastest)
+toolbox.router.get("img/*.jpg", toolbox.fastest)
+toolbox.router.get("img/*.svg", toolbox.fastest)
+toolbox.router.get("img/*.gif", toolbox.fastest)
+toolbox.router.get("img/*.png", toolbox.fastest)
+toolbox.router.get("manifest.json", toolbox.networkFirst)
