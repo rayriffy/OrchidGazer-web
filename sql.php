ad340cf2ba7cceb5dbe5323a2c58a3f168421164
Ray Riffy <rayriffy@gmail.com>
2017-06-23 08:08:05 +0700

    .

--- sql.php
+++ sql.php
@@ -1,6 +1,6 @@
 <?
   function sql_connect() {
-    $conn = mysql_connect('localhost','orchidgazer','riffy007') or die('ERR: Could not connect to sql');
+    $conn = mysql_connect('#','#','#') or die('ERR: Could not connect to sql');
     return $conn;
   }
 
