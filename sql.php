eea402d6570a4982f4baaa99a801327d93e6d873
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-03-15 10:26:45 +0700

    .

--- sql.php
+++ sql.php
@@ -1,6 +1,6 @@
 <?
   function sql_connect() {
-    $conn = mysql_connect('vps.rayriffy.com','orchidgazer','riffy007') or die('ERR: Could not connect to sql');
+    $conn = mysql_connect('localhost','orchidgazer','Fj87swkft1') or die('ERR: Could not connect to sql');
     return $conn;
   }
 
